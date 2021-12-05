c06a13007930a60462923f257b2ebdaa040f8e9f
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-05 22:01:21 +0300

    add tasks

--- src/App.js
+++ src/App.js
@@ -3,9 +3,35 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Tasks from "./components/Tasks";
+import AdminTask from "./components/AdminTask";
+import axios from "axios";
 
 function App() {
   const [login, setLogin] = useState(false);
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    getRoles();
+  }, [login]);
+
+  const getRoles = async () => {
+    try {
+      const result = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/getRoles`
+      );
+
+      const role = localStorage.getItem("role");
+
+      const found = result.data.find((item) => {
+        return item._id === role;
+      });
+      if (!found) return;
+      if (found.role === "admin") setRole("admin");
+      else setRole("user");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -19,10 +45,12 @@ function App() {
         </>
       ) : (
         <>
-          <Tasks />
+          {role === "user" ? <Tasks /> : <AdminTask />}
+
           <button
             onClick={() => {
               setLogin(false);
+              localStorage.clear();
             }}
           >
             logout{" "}
--- src/components/AdminTask/index.js
+++ src/components/AdminTask/index.js
@@ -2,29 +2,44 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Tasks() {
+  const [users, setUsers] = useState([]);
   const [items, setItems] = useState([]);
   const [taskAdd, setTaskAdd] = useState("");
   const [taskUpdate, setTaskUpdate] = useState("");
   const [createTask, setCreateTask] = useState(1);
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
 
   useEffect(() => {
     getAllItems();
+    getAllUsers();
   }, []);
 
   useEffect(() => {
     getAllItems();
-  }, [createTask]);
+  }, [createTask, user]);
+
+  const getAllUsers = async () => {
+    try {
+      const userId = localStorage.getItem("ID");
+      const result = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/users`,
+        { params: { userId }, headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      setUsers(result.data);
+      console.log(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const getAllItems = async () => {
     try {
       const userId = localStorage.getItem("ID");
       const result = await axios.get(
-        `${process.env.REACT_APP_BASE_URL}/task/todos/${userId}`,
-        {
-          params: { userId: userId },
-          headers: { Authorization: `Bearer ${token}` },
-        }
+        `${process.env.REACT_APP_BASE_URL}/task/todos/${user}`,
+        { params: { userId }, headers: { Authorization: `Bearer ${token}` } }
       );
 
       setItems(result.data);
@@ -52,7 +67,6 @@ export default function Tasks() {
 
   const updateTask = async (taskId, userId) => {
     try {
-      //const userId = localStorage.getItem("ID");
       const result = await axios.put(
         `${process.env.REACT_APP_BASE_URL}/task/todoUpdate`,
         { taskId, userId, taskName: taskUpdate },
@@ -83,6 +97,20 @@ export default function Tasks() {
 
   return (
     <div>
+      <h1> Users: </h1>
+      <select
+        name="user"
+        id="user"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setUser(e.target.value);
+        }}
+      >
+        {users.map((item) => {
+          return <option value={item._id}>{item.email}</option>;
+        })}
+      </select>
+
       <h1>add task: </h1>
       <input
         type="text"
