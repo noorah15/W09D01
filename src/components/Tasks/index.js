5014d751934f99d4c2cd26db4bde27ef38d59306
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-05 16:46:10 +0300

    add create, update, delete and get tasks in Task component

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -3,10 +3,17 @@ import axios from "axios";
 
 export default function Tasks() {
   const [items, setItems] = useState([]);
+  const [taskAdd, setTaskAdd] = useState("");
+  const [taskUpdate, setTaskUpdate] = useState("");
+  const [createTask, setCreateTask] = useState(1);
 
   useEffect(() => {
     getAllItems();
   }, []);
+
+  useEffect(() => {
+    getAllItems();
+  }, [createTask]);
 
   const getAllItems = async () => {
     try {
@@ -23,14 +30,75 @@ export default function Tasks() {
     }
   };
 
+  const addTask = async () => {
+    try {
+      const userID = localStorage.getItem("ID");
+      const result = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/task/create`,
+        { name: taskAdd, user: userID }
+      );
+      console.log(result.data);
+      setCreateTask(createTask + 1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const updateTask = async (taskId, userId) => {
+    try {
+      const result = await axios.put(
+        `${process.env.REACT_APP_BASE_URL}/task/todoUpdate`,
+        { taskId, userId, taskName: taskUpdate }
+      );
+      console.log(result.data);
+      setCreateTask(createTask + 1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteTask = async (taskId, userId) => {
+    try {
+      const result = await axios.delete(
+        `${process.env.REACT_APP_BASE_URL}/task/todoDel`,
+        { data: { userId, taskId } }
+      );
+      console.log(result.data);
+      setCreateTask(createTask + 1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1>add task: </h1>
+      <input
+        type="text"
+        name="task"
+        onChange={(e) => setTaskAdd(e.target.value)}
+      />
+      <button onClick={() => addTask()}> Add </button>
 
       <h1>Tasks</h1>
-      {items.map((item) => {
-        <h1>{item.name} vvv</h1>;
-      })}
+      {items.map((item) => (
+        <>
+          <h1>{item.name}</h1>
+          <input
+            type="text"
+            name="task"
+            onChange={(e) => setTaskUpdate(e.target.value)}
+          />
+          <button onClick={() => updateTask(item._id, item.user)}>
+            {" "}
+            update{" "}
+          </button>
+          <button onClick={() => deleteTask(item._id, item.user)}>
+            {" "}
+            delete{" "}
+          </button>
+        </>
+      ))}
     </div>
   );
 }
